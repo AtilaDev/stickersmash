668280c47866c84d868d9b2be2c06ca01f6f45fb
Leandro Favre <atiladevelop@gmail.com>
2025-01-15 18:28:18 -0300

    app done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,11 +5,11 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
